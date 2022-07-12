10fcc1a4478c7768a39a3886d39b6ba2ae9c9dd2
qaser <huji@mail.ru>
2022-07-12 08:19:43 +0500

    main: изменил адрес в api

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('./middlewares/cors');
+const helmet = require('helmet');
+// const cors = require('./middlewares/cors');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const { registerValid, loginValid } = require('./middlewares/validationJoi');
@@ -10,13 +11,14 @@ const { createUser, login } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env; // Слушаем 3000 порт
+const { PORT = 3000 } = process.env; // Слушаем 3000 порт
 const app = express();
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors());
+app.use(helmet());
+// app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
