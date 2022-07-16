2a41e0c0abee6f766f0072060ecc6867a6bf8f7a
qaser <huji@mail.ru>
2022-07-16 10:13:08 +0500

    main: fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,10 +6,13 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   throw new UnauthorizedError('Необходима авторизация');
+  // }
+  const token = authorization.replace('Bearer ', '');
+  if (!token) {
     throw new UnauthorizedError('Необходима авторизация');
   }
-  const token = String(authorization).replace('Bearer ', '');
 
   let payload;
 
