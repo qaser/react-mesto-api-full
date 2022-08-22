c0c1bb2fcb09ebd31b571794bdc1124f704da4ca
qaser <huji@mail.ru>
2022-08-22 22:36:20 +0500

    main: подготовка к ревью

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,10 +3,8 @@ const allowedCors = [
   'https://huji.students.nomoredomains.xyz',
   'http://api.huji.students.nomorepartiesxyz.ru',
   'https://api.huji.students.nomorepartiesxyz.ru',
-  'https://localhost:3000',
-  'http://localhost:3000',
-  'https://localhost:3001',
-  'http://localhost:3001',
+  'localhost:3000',
+  'localhost:3001',
 ];
 
 // eslint-disable-next-line consistent-return
@@ -18,7 +16,7 @@ module.exports = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   if (methodHttp === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
