2c88dda04945ea7835b9995f63fdc2bfd682bfb3
qaser <huji@mail.ru>
2022-08-22 22:03:41 +0500

    main: подготовка к ревью

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,23 +9,26 @@ const allowedCors = [
   'http://localhost:3001',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 // eslint-disable-next-line consistent-return
-module.exports = ((req, res, next) => {
+module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  res.header('Access-Control-Allow-Credentials', 'true');
+
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.status(200).end();
+    res.end();
   }
 
   next();
-});
+};
